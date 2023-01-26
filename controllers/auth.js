83d1c9d6c24724ae7590a2dd07e87f5dfef8a31c
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2023-01-26 10:15:34 +0100

    fixed some bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require("bcrypt");
 const { createToken, attachCookiesToRes } = require("../utils/jwt");
 const crypto = require("crypto");
 const Token = require("../models/Token");
-const sendEmail = require("../utils/sendEmail");
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -17,29 +16,10 @@ const signup = async (req, res) => {
     throw new BadRequest("An account with this email already exists!");
   }
 
-  const verificationToken = crypto.randomBytes(64).toString("hex");
-  const origin = req.headers.host;
-  req.body.verificationToken = verificationToken;
   const user = await User.create(req.body);
-  await sendEmail(name, email, verificationToken, origin);
-  res.status(StatusCodes.CREATED).json({ token: verificationToken, email });
-};
-
-const verifyEmail = async (req, res) => {
-  const { verificationToken: token, email } = req.body;
-  const user = await User.findOne({ email });
-
-  if (user.verificationToken !== token) {
-    throw new Unauthenticated("Email verification failed!");
-  }
-
-  user.isVerified = true;
-  user.verified = Date.now();
-  user.verificationToken = "";
-  await user.save();
   res
-    .status(StatusCodes.OK)
-    .json({ status: "Success!", msg: "Email verified!" });
+    .status(StatusCodes.CREATED)
+    .json({ user: { userId: user._id, name: user.name } });
 };
 
 const login = async (req, res) => {
@@ -48,9 +28,6 @@ const login = async (req, res) => {
     throw new BadRequest("Pleas fill out all fields!");
   }
   const user = await User.findOne({ email });
-  if (!user.isVerified) {
-    throw new Unauthenticated("Please verify your email address!");
-  }
   if (!user || !(await bcrypt.compare(password, user.password))) {
     throw new Unauthenticated("Invalid credentials!");
   }
@@ -91,4 +68,4 @@ const logout = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "logged out!" });
 };
 
-module.exports = { signup, login, logout, verifyEmail };
+module.exports = { signup, login, logout };
