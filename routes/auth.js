a081ae4577e097827e41841c7800b481bcde559f
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2023-01-26 10:16:52 +0100

    updated routes structure

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const { signup, login, logout, verifyEmail } = require("../controllers/auth");
+const { signup, login, logout } = require("../controllers/auth");
 const router = express.Router();
 const authUser = require("../middlewares/authMiddleware");
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/verify-email", verifyEmail);
 router.get("/logout", authUser, logout);
 module.exports = router;
