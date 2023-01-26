29c7fdc6d49ce93c494c0a40cd50b91e57ddbfde
Abdeladim Choukri <choukri.abdeladim@gmail.com>
2023-01-26 10:17:18 +0100

    fixed some small bugs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("express-async-errors");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const connectDB = require("./db/connect");
 const notFound = require("./middlewares/notFound");
 const authRouter = require("./routes/auth");
